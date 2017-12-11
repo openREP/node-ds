c4af0f3e6bcdb2bfeafad1992915fa49f31d11e5
Zhiquan Yeo <zyeo8@bloomberg.net>
2017-12-11 15:07:15 -0500

    More fixes

--- examples/ds-robot-example.js
+++ examples/ds-robot-example.js
@@ -3,7 +3,16 @@ const DS2016Protocol = require('../lib/protocols/ds-2016/ds-protocol-2016');
 
 var dsRobotClient = new DriverStationRobot(new DS2016Protocol());
 
+dsRobotClient.on('shouldReboot', () => {
+    console.log('Asked to reboot');
+});
+
+dsRobotClient.on('joysticksUpdated', (sticks) => {
+    console.log(sticks);
+});
+
 console.log('Starting DS Robot');
 dsRobotClient.start();
 
 dsRobotClient.robotCode = true;
+dsRobotClient.voltage = 12.5;
